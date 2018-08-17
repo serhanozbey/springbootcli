155458f78010930878122c9fb6f414b8be213713
serhanozbey <ozbey.serhan@gmail.com>
2018-08-17 14:24:49 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/commands/SelfCheckCommands.java
@@ -0,0 +1,19 @@
+package com.example.demo.commands;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class SelfCheckCommands {
+
+    @ShellMethod("Shows ID")
+    public int showId(){
+        return 0;
+    }
+    @ShellMethod(value = "Shows users name")
+    public String showName(){
+        return "serhan";
+    }
+
+}
